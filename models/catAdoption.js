5b2c937dafdc66a8f65a5753949d39c4e62eda6e
Glitch (topic-02-lab-rb-25) <none>
2025-02-25 13:36:10 +0000

    🚛🌼 Checkpoint ./models/catAdoption.js:74048282/1356

--- models/catAdoption.js
+++ models/catAdoption.js
@@ -1,27 +1,25 @@
 
-
 'use strict';
 
 import logger from '../utils/logger.js';
 import Functions from './functions.js';
+ 
 
 const catAdoption = {
   
   
-  store: new catAdoption('./models/catAdoption.json', {info : {}, featuredCats: [] }),
-   collection: 'info',
+  store: new Functions('./models/catAdoption.json', {info: {}}), 
+  collection: 'info',
   array: 'featuredCats',
-  
-  
+
   getCatInfo() {
     return this.store.findAll(this.collection);
   },
-  
+
   getFeaturedCats() {
     return this.store.findAll(this.array);
   }
-
   
-  };
+};
 
 export default catAdoption;
