3f2f70c5d5516f030c29e36d46c76af1d74ef40f
Glitch (topic-02-lab-rb-25) <none>
2025-03-03 15:33:07 +0000

    🚃🐢 Checkpoint ./controllers/about.js:74048282/181 ./views/about.hbs:74048282/11

--- controllers/about.js
+++ controllers/about.js
@@ -1,13 +1,16 @@
 'use strict';
 
 import logger from "../utils/logger.js";
+import meetTeam from '../models/meetTeam.js';
 
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
     
     const viewData = {
-      title: "Cat Adoption Center "
+      title: "Cat Adoption Center ",
+      team: meetTeam.getTeamInfo()
+      
     };
     
     response.render("about", viewData);
