47608ee4f5c82d5d1cfad5c7ed82a33adf61a021
Glitch (topic-02-lab-rb-25) <none>
2025-03-26 11:36:27 +0000

    🎙🏐 Checkpoint ./views/partials/addBreed.hbs:74048282/778 ./controllers/dashboard.js:74048282/531

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,6 +2,7 @@
 
 import logger from "../utils/logger.js";
 import mycollection from '../models/mycollection.js';
+import { v4 as uuidv4 } from 'uuid';
 
 
 
@@ -11,6 +12,23 @@ const dashboard = {
     //logs when the page is loading
     logger.info("Dashboard page loading!");
     
+    
+    addCatBreed(request, response) {
+    const newCatBreed = {
+      id: uuidv4(),
+      breed: request.body.breed,
+	    name: request.body.name,
+      age: request.body.age,
+      Lifespan
+      Size
+      Weight
+      Colour
+      songs: [],
+    };
+    myCollection.addCatBreed(newCatBreed);
+    response.redirect('/dashboard');
+},
+
     //Data is passed to the view 
     const viewData = {
       title: "Cat Adoption Center Dashboard",
