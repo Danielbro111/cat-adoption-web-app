8f3454c5f21c329d75e4f6a03c4c011d8a1d3fab
Glitch (topic-02-lab-rb-25) <none>
2025-04-07 12:41:39 +0000

    😄👑 Checkpoint ./controllers/dashboard.js:74048282/1766 ./models/mycollection.json:74048282/10886

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,28 +3,42 @@
 import logger from "../utils/logger.js";
 import mycollection from '../models/mycollection.js';
 import { v4 as uuidv4 } from 'uuid';
+import accounts from './accounts.js';
+
 
 const dashboard = {
   // Creates and renders page
   createView(request, response) {
     // Logs when the page is loading
     logger.info("Dashboard page loading!");
-
+const loggedInUser = accounts.getCurrentUser(request);
+    if (loggedInUser) {
     // Data is passed to the view
     const viewData = {
       title: "Cat Adoption Center Dashboard",
-      catBreeds: mycollection.getAllCats()
+      catBreeds: mycollection.getUserCatBreeds(loggedInUser.id),
+      fullname: loggedInUser.firstName + ' ' + loggedInUser.lastName,
     };
     
     // Data is passed to the view
+      logger.info('about to render' + viewData.catBreeds);
     logger.debug(viewData.catBreeds);
 
     // Renders the view with the data
     response.render('dashboard', viewData);
-  },
+  }
+    else response.redirect('/');
 
+   },
+  
+  
   // Adds a new cat breed
   addCatBreed(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
+    logger.debug(loggedInUser.id);
+    const timestamp = new Date();
+    
+    
     const newCatBreed = {
       id: uuidv4(),
       breed: request.body.breed,
@@ -36,6 +50,7 @@ const dashboard = {
       colour: request.body.colour,
       personality: request.body.personality,
       cats: [],
+      date: timestamp
     };
 
     mycollection.addCatBreed(newCatBreed);
